5089e9fb988a495c0ffd02efea3c0574de0d9353
Nikhil Prasad <contact.nikhilprasad@gmail.com>
2026-07-19 07:10:57 +0530

    feat(footer): add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import '../styles/footer.css';
+
+export default function Footer() {
+  return (
+    <footer className="app-footer">
+      <p className="app-footer-text">
+        🌸DPI♾️ can make mistakes. Consider verifying important information.
+      </p>
+    </footer>
+  );
+}
